00b4e3a889049d3dc42a64bd723efece3c110f54
echoiron <echioron@gmail.com>
2020-08-18 14:30:58 +0800

    add scripts

---
+++ scripts/notepad2/1_Replace_Notepad.bat
@@ -0,0 +1,24 @@
+@echo off
+chcp 65001 1>nul 2>nul
+net.exe session 1>NUL 2>NUL && (
+    goto as_admin
+) || (
+    goto not_admin
+)
+
+:as_admin
+cd /d "%~dp0"
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\notepad.exe" /v "Debugger" /d "\"%~dp0Notepad2.exe\" /z" /f
+cls
+echo.
+echo Replaced successfully by echoiron
+echo.
+
+goto end
+
+:not_admin
+echo Please run as administrator
+
+:end
+echo Press any key to end
+pause 1>nul 2>nul
