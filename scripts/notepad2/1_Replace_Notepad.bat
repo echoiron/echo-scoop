609ccec49b7b99fb21621514af7757ba74b009f1
echoiron <echioron@gmail.com>
2020-08-18 15:09:42 +0800

    update scripts

--- scripts/notepad2/1_Replace_Notepad.bat
+++ scripts/notepad2/1_Replace_Notepad.bat
@@ -1,5 +1,9 @@
 @echo off
 chcp 65001 1>nul 2>nul
+
+rem :: upgrade to administrator
+(cd/d "%~dp0")&(cacls "%SystemDrive%\System Volume Information" >nul 2>&1)||(start "" mshta vbscript:CreateObject^("Shell.Application"^).ShellExecute^("%~snx0"," %*","","runas",1^)^(window.close^)&exit /b)
+
 net.exe session 1>NUL 2>NUL && (
     goto as_admin
 ) || (
