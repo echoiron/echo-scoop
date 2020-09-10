d419869776ca0469289bb0ae534c63a9ffb2be79
echoiron <echioron@gmail.com>
2020-09-10 14:27:06 +0800

    update script

--- scripts/notepad3/1_replace_notepad.bat
+++ scripts/notepad3/1_replace_notepad.bat
@@ -1,28 +1,39 @@
 @echo off
-chcp 65001 1>nul 2>nul
-
-rem :: upgrade to administrator
-(cd/d "%~dp0")&(cacls "%SystemDrive%\System Volume Information" >nul 2>&1)||(start "" mshta vbscript:CreateObject^("Shell.Application"^).ShellExecute^("%~snx0"," %*","","runas",1^)^(window.close^)&exit /b)
-
-net.exe session 1>NUL 2>NUL && (
-    goto as_admin
-) || (
-    goto not_admin
+:: batchgotadmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 )
 
-:as_admin
-cd /d "%~dp0"
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:: batchgotadmin
+
+:: handle
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\notepad.exe" /v "Debugger" /d "\"%~dp0Notepad3.exe\" /z" /f
 cls
 echo.
 echo Replaced successfully by echoiron
 echo.
-
-goto end
-
-:not_admin
-echo Please run as administrator
-
-:end
 echo Press any key to end
 pause 1>nul 2>nul
+:: handle
